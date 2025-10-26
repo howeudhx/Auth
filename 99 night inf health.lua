0ab86f51d16507bfae5a678457c28c6ff50fc8a8
Kawnew <surkesus@gmail.com>
2025-10-27 02:26:37 +0700

    Create 99 night inf health.lua

---
+++ 99 night inf health.lua
@@ -0,0 +1,11 @@
+--[[ This File loadstring by AuthGuard
+Feature :
+- infinity health 
+- speed bypass (speed 75)
+- optimize fps
+
+enjoy bitch 
+]]
+
+
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/d0a86d51c26b418a927d40e53a3c9a39"))()
