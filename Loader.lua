4a2fe63f4cb27431300541389c3ccbdb13d2189c
Kawnew <surkesus@gmail.com>
2025-10-26 19:36:15 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,15 @@
-print("In dev")
+--[[
+Basically this script has been modify fir Blox fruit Helper and Bypass Anti-Cheat 
+but We thought it might be boring
+so we added a variety of functions such as performance 
+adjustments and even help with other maps......
+
+]]
+
+
+-- AuthGuard Loader (Key Need..)
+
+loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/89e94196154648f3a6d469605f01e19e"))()
+
+
+--in dev For collecting loader 
